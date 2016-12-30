535696e850fbda70f499962aebbc86164ece97a6
kristyduong <kristyduong5@gmail.com>
2016-12-30 00:15:06 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var socialIcons = document.getElementById("social-icons");
 if (socialIcons) {
 	socialIcons.innerHTML = 
-"<a href='https://www.linkedin.com/in/kristyduong5' id='linkedinLink' class='icon-3x icon-linkedin-sign' aria-label='LinkedIn'></a> " +
-"<a href='https://github.com/kristyduong' id='githubLink' class='icon-3x icon-github-sign' aria-label='Github'></a> ";
+"<a href='https://www.linkedin.com/in/kristyduong5' id='linkedinLink' class='icon-3x icon-linkedin-sign link-no-underline' aria-label='LinkedIn'></a> " +
+"<a href='https://github.com/kristyduong' id='githubLink' class='icon-3x icon-github-sign link-no-underline' aria-label='Github'></a> ";
 }
