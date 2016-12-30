d83cc72a26ce4a0e5cdc76a1f74be55c24b5079e
kristyduong <kristyduong5@gmail.com>
2016-12-29 23:36:21 -0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+var socialIcons = document.getElementById("social-icons");
+if (socialIcons) {
+	socialIcons.innerHTML = 
+"<a href='https://www.linkedin.com/in/kristyduong5' id='linkedinLink' class='icon-3x icon-linkedin-sign link-no-underline w3-hover-text-strawberry-red' aria-label='LinkedIn'></a> " +
+"<a href='https://github.com/kristyduong' id='githubLink' class='icon-3x icon-github-sign link-no-underline w3-hover-text-strawberry-red' aria-label='Github'></a> ";
+}
